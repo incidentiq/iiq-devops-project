2e4c6ef1f4b16a2c0933bb0e1888e728b1eed864
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro.local>
2024-09-10 00:55:58 -0600

    fixing file typo and tf vars file

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,13 +1,30 @@
-terraform {
-  required_version = ">=1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
+variable "node_count" {
+  default = 1
 }
 
-provider "azurerm" {
-  features {}
+variable "container_registry_name" {
+  default = "iisdevopsprojectcontainerregistry"
+}
+
+variable "cluster_name" {
+  default = "iis-devops-project-aks-cluster"
+}
+
+variable "dns_prefix" {
+  default = "iis-aks"
+}
+
+variable "resource_group_location" {
+  default     = "eastus2"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "iis-devops-project-rg"
+  description = "Name of the Azure resource group that the AKS cluster will exist within."
+}
+
+// Path to the public key used for authN with the AKS cluster (using local path for demo, but should be pulled in via CD pipeline)
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
 }
